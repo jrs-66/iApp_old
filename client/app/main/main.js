92a3d5620f095cb95a5e380e0eeb38fd9369cb86
James Staub <jamesstaub@Jamess-MacBook-Pro.local>
2015-05-06 11:05:54 -0400

    first project commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('menuApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
